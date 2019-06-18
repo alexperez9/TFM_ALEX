51f52080352fd3b2f0e2aa50da1754093ac3a168
alexperez9 <alexperezcasas@gmail.com>
2019-06-19 00:50:44 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,96 @@
-// Get references to UI elements
-let connectButton = document.getElementById('connect');
-let disconnectButton = document.getElementById('disconnect');
-let terminalContainer = document.getElementById('terminal');
-let sendForm = document.getElementById('send-form');
-let inputField = document.getElementById('input');
-
-// Connect to the device on Connect button click
-connectButton.addEventListener('click', function() {
-  connect();
-});
-
-// Disconnect from the device on Disconnect button click
-disconnectButton.addEventListener('click', function() {
-  disconnect();
-});
-
-// Handle form submit event
-sendForm.addEventListener('submit', function(event) {
-  event.preventDefault(); // Prevent form sending
-  send(inputField.value); // Send text field contents
-  inputField.value = '';  // Zero text field
-  inputField.focus();     // Focus on text field
-});
-
-// Launch Bluetooth device chooser and connect to the selected
-function connect() {
-  //
+if (!navigator.bluetooth) {
+  alert('Sorry, your browser doesn\'t support Bluetooth API');
 }
 
-// Disconnect from the connected device
-function disconnect() {
-  //
+const MY_BLUETOOTH_NAME = 'CC41-A';
+const SEND_SERVICE = 0xFFE0;
+const SEND_SERVICE_CHARACTERISTIC = 0xFFE1;
+
+const controlButtonsListElements = document.querySelectorAll('.control-buttons > li');
+const connectButton = document.getElementById('connectButton');
+const disconnectButton = document.getElementById('disconnectButton');
+const lightOffButton = document.getElementById('lightOff');
+const toggleRedLightButton = document.getElementById('toggleRedLight');
+const toggleBlueLightButton = document.getElementById('toggleBlueLight');
+const toggleGreenLightButton = document.getElementById('toggleGreenLight');
+const runBlinkLightButton = document.getElementById('runBlinkLight');
+
+let toggleLigthCharacteristic;
+let myDevice;
+
+connectButton.addEventListener('pointerup', connectButtonPointerUpHandler);
+
+function connectButtonPointerUpHandler() {
+  navigator.bluetooth.requestDevice({
+    filters:
+      [
+        { name: MY_BLUETOOTH_NAME },
+        { services: [SEND_SERVICE] },
+      ]
+  })
+    .then(device => {
+      myDevice = device;
+
+      return device.gatt.connect();
+    })
+    .then(server => server.getPrimaryService(SEND_SERVICE))
+    .then(service => service.getCharacteristic(SEND_SERVICE_CHARACTERISTIC))
+    .then(characteristic => {
+      toggleLigthCharacteristic = characteristic;
+
+      toggleButtonsVisible();
+      toggleItemsEventListeners('addEventListener');
+    })
+    .catch(error => {
+      console.error(error);
+    });
 }
 
-// Send data to the connected device
-function send(data) {
-  //
+function lightOffButtonClickHandler() {
+  return toggleLigthCharacteristic.writeValue(Uint8Array.of(0));
+}
+
+function toggleLightButtonClickHandler(event) {
+  const code = Number(event.target.dataset.code);
+
+  if (code === 1) {
+    toggleLigthCharacteristic.writeValue(Uint8Array.of(code));
+
+    return;
+  }
+
+  toggleLigthCharacteristic.readValue()
+    .then(currentCode => {
+      const convertedCode = currentCode.getUint8(0);
+
+      toggleLigthCharacteristic.writeValue(Uint8Array.of(convertedCode === code ? 0 : code));
+    });
+}
+
+function toggleButtonsVisible() {
+  Array.prototype.forEach.call(controlButtonsListElements, listElement => {
+    listElement.classList.toggle('visible');
+  });
+}
+
+function disconnectButtonClickHandler() {
+  lightOffButtonClickHandler()
+    .then( () => {
+      myDevice.gatt.disconnect();
+
+      toggleItemsEventListeners('removeEventListener');
+      toggleButtonsVisible();
+
+      toggleLigthCharacteristic = undefined;
+      myDevice = undefined;
+    });
+}
+
+function toggleItemsEventListeners(action) {
+  disconnectButton[action]('click', disconnectButtonClickHandler);
+  lightOffButton[action]('click', lightOffButtonClickHandler);
+  runBlinkLightButton[action]('click', toggleLightButtonClickHandler);
+  toggleGreenLightButton[action]('click', toggleLightButtonClickHandler);
+  toggleRedLightButton[action]('click', toggleLightButtonClickHandler);
+  toggleBlueLightButton[action]('click', toggleLightButtonClickHandler);
 }
